82229f00be78d4f823d4dc14a73017bd609b4ba1
Ken Wu <kenwu1009us@gmail.com>
2023-12-30 16:14:43 -0800

    refactor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
-from calculator import calculation
+from calculator import calculate_capacity
+
 
 # Create the app object
 app = Flask(
@@ -14,19 +15,24 @@ app = Flask(
 # Define calculator
 @app.route("/")
 def home():
+    """
+    Render the homepage with the capacity calculator form.
+    """
     return render_template("index.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    sprintDays = request.form["a"]
-    hoursPerDay = request.form["b"]
-    meetingHours = request.form["c"]
-    ptoHours = request.form["d"]
+    """
+    Handle the capacity calculation request and display the result.
+    """
+    sprint_days = request.form["sprint_days"]
+    hours_per_day = request.form["hours_per_day"]
+    meeting_hours = request.form["meeting_hours"]
+    pto_hours = request.form["pto_hours"]
 
-    res = calculation(sprintDays, hoursPerDay, meetingHours, ptoHours)
-
-    return render_template("index.html", prediction_text=res)
+    result = calculate_capacity(sprint_days, hours_per_day, meeting_hours, pto_hours)
+    return render_template("index.html", prediction_text=result)
 
 
 if __name__ == "__main__":
--- calculator.py
+++ calculator.py
@@ -1,16 +1,27 @@
 from decimal import Decimal
 
 
-def calculation(sprint_days, hours_per_day, meeting_hours, pto_hours):
-    lst = [sprint_days, hours_per_day, meeting_hours, pto_hours]
-    for i in range(len(lst)):
-        try:
-            lst[i] = float(lst[i])
-        except:
-            text = "Please enter a valid number."
-            return text
+def calculate_capacity(sprint_days, hours_per_day, meeting_hours, pto_hours):
+    """
+    Calculate the available working capacity.
 
-    res = round(((((lst[0] * lst[1]) * 0.9) - (lst[2] * 1.1)) - lst[3]) * 2) / 2
-    text = f"Your capacity is {res} hours."
+    Args:
+        sprint_days (str): Number of sprint days.
+        hours_per_day (str): Number of working hours per day.
+        meeting_hours (str): Number of hours spent in meetings.
+        pto_hours (str): Number of PTO (Paid Time Off) hours.
 
-    return text
+    Returns:
+        str: The calculated capacity in hours or an error message.
+    """
+    inputs = [sprint_days, hours_per_day, meeting_hours, pto_hours]
+
+    try:
+        inputs = [float(item) for item in inputs]
+    except ValueError:
+        return "Please enter valid numbers."
+
+    capacity = (
+        round((((inputs[0] * inputs[1]) * 0.9) - (inputs[2] * 1.1) - inputs[3]) * 2) / 2
+    )
+    return f"Your capacity is {capacity} hours."
